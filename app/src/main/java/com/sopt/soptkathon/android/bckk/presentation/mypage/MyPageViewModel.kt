dae0747b79cb7bc75ab8ff88d892c366f3d4d3c5
Junseo511 <prime54@g.skku.edu>
2023-11-26 02:40:33 +0900

    [FEAT]: create mypage

--- app/src/main/java/com/sopt/soptkathon/android/bckk/presentation/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/sopt/soptkathon/android/bckk/presentation/mypage/MyPageViewModel.kt
@@ -1,4 +1,32 @@
 package com.sopt.soptkathon.android.bckk.presentation.mypage
 
-class MyPageViewModel {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sopt.soptkathon.android.bckk.data.api.ServicePool
+import com.sopt.soptkathon.android.bckk.data.api.model.UserResponse
+import kotlinx.coroutines.launch
+
+class MyPageViewModel : ViewModel() {
+
+    private val _userDto = MutableLiveData<UserResponse.UserDto>()
+    val userDto: LiveData<UserResponse.UserDto> get() = _userDto
+
+    private val _postDtoList = MutableLiveData<List<UserResponse.UserDto.PostDto>>()
+    val postDtoList: LiveData<List<UserResponse.UserDto.PostDto>> get() = _postDtoList
+
+    fun fetchData(ssaId: String) {
+        viewModelScope.launch {
+            runCatching {
+                ServicePool.heungService.getUserInfo(ssaId)
+            }.onSuccess { response ->
+                _userDto.value = response.data.userDto
+                _postDtoList.value = response.data.userDto.postListDto
+            }.onFailure {
+                Log.e("NetworkTest", "error:$it")
+            }
+        }
+    }
 }
