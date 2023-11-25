bde23319c0648baaf26e15401b40462f8cf97eef
kangyuri1114 <yuri3334@gmail.com>
2023-11-26 00:34:37 +0900

    [ADD] 홈 액티비티 / 클릭 리스너 세팅

--- app/src/main/java/com/sopt/soptkathon/android/bckk/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/sopt/soptkathon/android/bckk/presentation/home/HomeViewModel.kt
@@ -8,4 +8,11 @@ class HomeViewModel : ViewModel() {
 
     private val _userInfoResult = MutableLiveData<Boolean>()
     val userInfoResult: LiveData<Boolean> get() = _userInfoResult
+    val isGoArticleButtonClicked = MutableLiveData<Boolean>()
+
+    // api로 뽑아야 하는 정보 : 티켓수만! 티켓수가 1개 이상이면 자랑하기 이동 가능, 버튼 활성화
+
+    fun onClickGoArticleBtn() {
+        isGoArticleButtonClicked.value = true
+    }
 }
