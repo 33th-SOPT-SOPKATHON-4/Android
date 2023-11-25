ae755d4f14486ab65e5cf3bb18176a9f486b75ba
kangyuri1114 <yuri3334@gmail.com>
2023-11-25 23:14:07 +0900

    [Add] 홈액티비티 추가

---
+++ app/src/main/java/com/sopt/soptkathon/android/bckk/presentation/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.sopt.soptkathon.android.bckk.presentation.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sopt.soptkathon.android.bckk.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
