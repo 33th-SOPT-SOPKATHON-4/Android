e1dddc3a61fab554e67095597834475fcab1953d
KwakEuiJin <yuijin0909@gmail.com>
2023-11-25 18:29:53 +0900

    Add git ignore && SampleAdapter

---
+++ app/src/main/java/com/sopt/soptkathon/android/bckk/presentation/SampelAdapter.kt
@@ -0,0 +1,21 @@
+package com.sopt.soptkathon.android.bckk.presentation
+
+//class SampleAdapter : ListAdapter<SampleData, SampleAdapter.SampleViewHolder>(SampleDiffCallback) {
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SampleViewHolder {
+//        return SampleViewHolder(ItemSampleBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+//    }
+//
+//    override fun onBindViewHolder(holder: SampleViewHolder, position: Int) {
+//        holder.bind(currentList[position])
+//    }
+//
+//    class SampleViewHolder(private val binding: ItemSampleBinding) : ViewHolder(binding.root) {
+//        fun bind(item: SampleData) {
+//        }
+//    }
+//
+//    companion object {
+//        private val SampleDiffCallback =
+//            DiffCallback<SampleData>(id = { old, new -> old.sample == new.sample })
+//    }
+//}
--- app/src/main/java/com/sopt/soptkathon/android/bckk/util/ViewExtension.kt
+++ app/src/main/java/com/sopt/soptkathon/android/bckk/util/ViewExtension.kt
@@ -1,8 +1,76 @@
 package com.sopt.soptkathon.android.bckk.util
 
+import android.graphics.Rect
 import android.view.View
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
 
-fun View.showSnack(message: String) {
-    Snackbar.make(this, message, Snackbar.LENGTH_SHORT).show()
+inline fun View.setOnSingleClickListener(
+    delay: Long = 500L,
+    crossinline block: (View) -> Unit,
+) {
+    var previousClickedTime = 0L
+    setOnClickListener { view ->
+        val clickedTime = System.currentTimeMillis()
+        if (clickedTime - previousClickedTime >= delay) {
+            block(view)
+            previousClickedTime = clickedTime
+        }
+    }
+}
+
+fun View.snackBar(anchorView: View, message: () -> String) {
+    Snackbar.make(anchorView, message(), Snackbar.LENGTH_SHORT).show()
+}
+
+class ItemDiffCallback<T : Any>(
+    val onItemsTheSame: (T, T) -> Boolean,
+    val onContentsTheSame: (T, T) -> Boolean,
+) : DiffUtil.ItemCallback<T>() {
+    override fun areItemsTheSame(
+        oldItem: T,
+        newItem: T,
+    ): Boolean = onItemsTheSame(oldItem, newItem)
+
+    override fun areContentsTheSame(
+        oldItem: T,
+        newItem: T,
+    ): Boolean = onContentsTheSame(oldItem, newItem)
+}
+
+class GridSpacingItemDecoration(
+    private val spanCount: Int,
+    private val spacing: Int,
+    private val includeEdge: Boolean,
+) : RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State,
+    ) {
+        val position = parent.getChildAdapterPosition(view) // item position
+        val column = position % spanCount // item column
+
+        if (!includeEdge) {
+            outRect.left =
+                spacing - column * spacing / spanCount // spacing - column * ((1f / spanCount) * spacing)
+            outRect.right =
+                (column + 1) * spacing / spanCount // (column + 1) * ((1f / spanCount) * spacing)
+
+            if (position < spanCount) { // top edge
+                outRect.top = spacing
+            }
+            outRect.bottom = spacing // item bottom
+        } else {
+            outRect.left = column * spacing / spanCount // column * ((1f / spanCount) * spacing)
+            outRect.right =
+                spacing - (column + 1) * spacing / spanCount // spacing - (column + 1) * ((1f / spanCount) * spacing)
+            if (position >= spanCount) {
+                outRect.top = spacing // item top
+            }
+        }
+    }
 }
