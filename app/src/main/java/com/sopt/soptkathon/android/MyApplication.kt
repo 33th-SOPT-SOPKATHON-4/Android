99e0f1463c2e15c4cd4ff11eff3851a88f950f86
KwakEuiJin <yuijin0909@gmail.com>
2023-11-25 23:01:11 +0900

    Add Application

---
+++ app/src/main/java/com/sopt/soptkathon/android/MyApplication.kt
@@ -0,0 +1,20 @@
+package com.sopt.soptkathon.android
+
+import android.app.Application
+import com.sopt.soptkathon.android.bckk.BuildConfig
+import com.sopt.soptkathon.android.bckk.data.api.ApiFactory
+import com.sopt.soptkathon.android.bckk.data.sharedpreference.SharedPreferenceContainer
+import com.sopt.soptkathon.android.bckk.data.sharedpreference.SharedPreferenceContainer.sharedPreferencesInstance
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        myApplicationInstance = this
+        sharedPreferencesInstance = SharedPreferenceContainer.getAppPreferences(this)
+        ApiFactory.retrofit = ApiFactory.getRetrofit(BuildConfig.BASE_URL)
+    }
+
+    companion object {
+        lateinit var myApplicationInstance: MyApplication
+    }
+}
--- app/src/main/java/com/sopt/soptkathon/android/bckk/presentation/main/MainActivity.kt
+++ app/src/main/java/com/sopt/soptkathon/android/bckk/presentation/main/MainActivity.kt
@@ -2,6 +2,9 @@ package com.sopt.soptkathon.android.bckk.presentation.main
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import coil.load
 import com.sopt.soptkathon.android.bckk.R
 import com.sopt.soptkathon.android.bckk.base.BindActivity
@@ -14,8 +17,24 @@ class MainActivity : BindActivity<ActivityMainBinding>() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
+            val splashScreenView = splashScreenViewProvider.view
+            val fadeOut = AnimationUtils.loadAnimation(this, R.anim.fade_out)
+
+            fadeOut.setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation) {}
+                override fun onAnimationEnd(animation: Animation) {
+                    // 애니메이션 종료 후 스플래시 스크린 제거
+                    splashScreenViewProvider.remove()
+                }
+                override fun onAnimationRepeat(animation: Animation) {}
+            })
+            splashScreenView.startAnimation(fadeOut)
+        }
         binding.ivMain.load(R.drawable.ic_launcher_background)
     }
 }
