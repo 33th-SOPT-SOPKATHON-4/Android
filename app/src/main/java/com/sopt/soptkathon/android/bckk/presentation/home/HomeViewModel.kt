03a7f5312cbbf41658237528c2b8a706f284482a
kangyuri1114 <yuri3334@gmail.com>
2023-11-26 01:09:49 +0900

    [FEAT] API 함수 틀 추가

--- app/src/main/java/com/sopt/soptkathon/android/bckk/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/sopt/soptkathon/android/bckk/presentation/home/HomeViewModel.kt
@@ -1,8 +1,12 @@
 package com.sopt.soptkathon.android.bckk.presentation.home
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sopt.soptkathon.android.bckk.data.api.ServicePool
+import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
 
@@ -11,6 +15,18 @@ class HomeViewModel : ViewModel() {
     val isGoArticleButtonClicked = MutableLiveData<Boolean>()
 
     // api로 뽑아야 하는 정보 : 티켓수만! 티켓수가 1개 이상이면 자랑하기 이동 가능, 버튼 활성화
+//    fun getUserInfo() {
+//        viewModelScope.launch {
+//            runCatching {
+//                ServicePool.heungService.getUserInfo("1")
+//            }.onSuccess {
+//                Log.d("NetworkTest", it)
+//            }.onFailure {
+//                Log.e("NetworkTest", "error:$it")
+//            }
+//        }
+//    }
+
 
     fun onClickGoArticleBtn() {
         isGoArticleButtonClicked.value = true
