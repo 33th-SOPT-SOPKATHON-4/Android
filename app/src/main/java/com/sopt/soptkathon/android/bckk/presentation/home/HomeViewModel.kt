1bfff3a893216ee87d977137ae9f2083f3fe7cd2
kangyuri1114 <yuri3334@gmail.com>
2023-11-26 01:22:01 +0900

    [FEAT] API 함수 추가

--- app/src/main/java/com/sopt/soptkathon/android/bckk/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/sopt/soptkathon/android/bckk/presentation/home/HomeViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sopt.soptkathon.android.bckk.data.api.ServicePool
+import com.sopt.soptkathon.android.bckk.data.api.model.UserResponse
 import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
@@ -15,18 +16,22 @@ class HomeViewModel : ViewModel() {
     val isGoArticleButtonClicked = MutableLiveData<Boolean>()
 
     // api로 뽑아야 하는 정보 : 티켓수만! 티켓수가 1개 이상이면 자랑하기 이동 가능, 버튼 활성화
-//    fun getUserInfo() {
-//        viewModelScope.launch {
-//            runCatching {
-//                ServicePool.heungService.getUserInfo("1")
-//            }.onSuccess {
-//                Log.d("NetworkTest", it)
-//            }.onFailure {
-//                Log.e("NetworkTest", "error:$it")
-//            }
-//        }
-//    }
-
+    fun getUserInfo() {
+        viewModelScope.launch {
+            runCatching {
+                ServicePool.heungService.getUserInfo("1") // TODO 유저 아이디 추가 String
+            }.onSuccess { userResponse ->
+                Log.d("NetworkTest", userResponse.toString())
+                _userInfoResult.value = true
+                val ticketCount = UserResponse().userDto.ticketCount
+                if (ticketCount > 0) {
+                    isGoArticleButtonClicked.value = true
+                }
+            }.onFailure {
+                Log.e("NetworkTest", "error:$it")
+            }
+        }
+    }
 
     fun onClickGoArticleBtn() {
         isGoArticleButtonClicked.value = true
